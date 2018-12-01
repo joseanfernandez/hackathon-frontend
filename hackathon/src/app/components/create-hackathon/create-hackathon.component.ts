6e76b20ca83226a41ca20096d883a4233722d287
Juan Antonio Moreno Valderrama <jamv999@gmail.com>
2018-12-01 18:55:44 +0100

    Cambios de aspecto a bootstra y añado validación

--- hackathon/src/app/components/create-hackathon/create-hackathon.component.ts
+++ hackathon/src/app/components/create-hackathon/create-hackathon.component.ts
@@ -9,11 +9,38 @@ import { Hackathon } from '../../interfaces/hackathon';
 export class CreateHackathonComponent implements OnInit {
 
   private _hackathon: Hackathon;
+  private _send = false;
   public get hackathon() { return this._hackathon; }
 
   constructor() { }
 
   ngOnInit() {
+    this.reset();
+  }
+
+  public submit(): void {
+    this._send = true;
+    this.checkValue();
+  }
+
+  private checkValue(): boolean {
+    return this.checkTitleValue() && this.checkTechnologyValue() &&
+        this.checkDateValue();
+  }
+
+  private checkTitleValue() {
+    return !this._send || this._hackathon.title !== '';
+  }
+
+  private checkTechnologyValue() {
+    return !this._send || this._hackathon.technology !== '';
+  }
+
+  private checkDateValue() {
+    return !this._send || this._hackathon.date !== '';
+  }
+
+  public reset(): void {
     this._hackathon = {
       id: '',
       title: '',
@@ -22,8 +49,4 @@ export class CreateHackathonComponent implements OnInit {
       notes: ''
     };
   }
-
-  public submit(): void {
-  }
-
 }
