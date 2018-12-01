f29eb15ce0d99def6a483727141b401aae958656
Jose Antonio Fernández García <32483839+joseanfernandez@users.noreply.github.com>
2018-12-01 22:34:52 +0100

    Update environment.ts

--- hackathon/src/environments/environment.ts
+++ hackathon/src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyCW7pE4cMXDHd91_VtcgFvSYVHquGhbunk',
-    authDomain: 'hackathon-frontend-88570.firebaseapp.com',
-    databaseURL: 'https://hackathon-frontend-88570.firebaseio.com',
-    projectId: 'hackathon-frontend-88570',
-    storageBucket: 'hackathon-frontend-88570.appspot.com',
-    messagingSenderId: '798827764570'
-  }
 };
 
